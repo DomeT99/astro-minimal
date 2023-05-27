6d59c28f4a7011b861254dbca6515143f1d46bbe
Dvalin <domenicotenace99@gmail.com>
2023-05-27 18:16:56 +0200

    feat: partial styiling navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue()],
+});
---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+function openBurger() {
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  $navbarBurgers.forEach((element) => {
+    element.addEventListener("click", () => {
+      const target = element.dataset.target;
+      const $target = document.getElementById(target) as HTMLElement;
+
+      element.classList.toggle("is-active");
+
+      $target.classList.toggle("is-active");
+    });
+  });
+}
+
+onMounted(() => {
+    openBurger();
+})
+</script>
+
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <p class="f-size-17">DOMENICO TENACE</p>
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbar-id"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbar-id" class="navbar-menu">
+      <div class="navbar-end">
+        <a class="navbar-item f-600">ABOUT</a>
+        <a class="navbar-item f-600"> BLOG</a>
+        <a class="navbar-item f-600"> OPEN SOURCE</a>
+      </div>
+    </div>
+  </nav>
+</template>
