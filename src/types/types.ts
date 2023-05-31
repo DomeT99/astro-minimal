3ae92a71978c210a140f326cf877cc20ba9b0b1e
Dvalin <domenicotenace99@gmail.com>
2023-05-31 12:41:52 +0200

    ref: refactoring code components

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export type SocialButton = {
+  icon: string;
+  url: string;
+};
+
+export type MenuOptions = {
+  label: string;
+  url: string;
+}
---
+++ src/utils/variables.ts
@@ -0,0 +1,35 @@
+import { SocialButton, MenuOptions } from "../types/types";
+
+export const socialButtons: SocialButton[] = [
+  {
+    icon: "eva:linkedin-outline",
+    url: "https://www.linkedin.com/in/domenico-tenace/",
+  },
+  {
+    icon: "eva:github-outline",
+    url: "https://github.com/DomeT99",
+  },
+  {
+    icon: "eva:twitter-outline",
+    url: "https://twitter.com/dome_tena99",
+  },
+];
+
+export const menuOptions: MenuOptions[] = [
+  {
+    label: "ABOUT",
+    url: "/about",
+  },
+  {
+    label: "BLOG",
+    url: "/blog",
+  },
+  {
+    label: "PROJECTS",
+    url: "/projects",
+  },
+  {
+    label: "OPEN SOURCE",
+    url: "/opensource",
+  },
+]
