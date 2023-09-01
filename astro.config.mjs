bf1a870f0ea3044d67e3a7253132a8fb0d9fad81
Domenico Tenace <domenicotenace99@gmail.com>
2023-09-01 19:18:05 +0200

    feat: setup mdx file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-export default defineConfig();
+export default defineConfig({
+  integrations: [mdx()],
+});
--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,4 +1,4 @@
-import {
+import type {
   SocialButton,
   MenuOptions,
   GitHubRepo
