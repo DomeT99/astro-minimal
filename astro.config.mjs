58f76d103ecb156b8867da804c63da9d5d47ee9a
Domenico Tenace <domenicotenace99@gmail.com>
2025-03-23 18:19:40 +0100

    feat: add internationalization support with i18next and create locale files for English and Italian

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import icon from "astro-icon";
+import astroI18next from "astro-i18next";
 
 export default defineConfig({
-  integrations: [vue(), icon()],
+  integrations: [vue(), icon(), astroI18next()],
 });
